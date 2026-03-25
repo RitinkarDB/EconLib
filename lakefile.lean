2862c7f9accf629e67fd65c3b1c0699a6829f8ae
Ritinkar Das Bhaumik <161712585+RitinkarDB@users.noreply.github.com>
2026-03-25 10:00:27 +0000

    Basic.lean commit + lakefiles

---
+++ lakefile.lean
@@ -0,0 +1,12 @@
+import Lake
+open Lake DSL
+
+package EconLib where
+  -- add package configuration options here
+
+require mathlib from git
+  "https://github.com/leanprover-community/mathlib4" @ "v4.28.0"
+
+@[default_target]
+lean_lib EconLib where
+  roots := #[`Basic]
